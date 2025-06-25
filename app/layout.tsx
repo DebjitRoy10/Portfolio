744ebdb7ecb080090f814911935f9848363c4cea
Debjit Roy <121539403+DebjitRoy10@users.noreply.github.com>
2025-06-25 13:28:06 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Debjit Roy',
+  description: 'See My Portfolio and connect with me',
   generator: 'v0.dev',
 }
 
